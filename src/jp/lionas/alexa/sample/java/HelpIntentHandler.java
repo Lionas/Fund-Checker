545e6f58807ceefa6462af5fec4ff5ee2f4f82f1
Naoki Seto <lionas.watanabe@gmail.com>
2018-04-03 00:16:49 +0900

    add custom skill classes

---
+++ src/jp/lionas/alexa/sample/java/HelpIntentHandler.java
@@ -0,0 +1,28 @@
+package jp.lionas.alexa.sample.java;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class HelpIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput handlerInput) {
+        return handlerInput.matches(intentName("AMAZON.HelpIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput) {
+        String speechText = "You can say hello to me!";
+        return handlerInput.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
+}
