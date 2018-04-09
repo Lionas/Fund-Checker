891f97d4ceb5c189b83e13210ade123c8fd8cc22
Naoki Seto <lionas.watanabe@gmail.com>
2018-04-10 00:13:14 +0900

    fix help speech text issue

--- src/jp/lionas/alexa/fundra/def/Message.kt
+++ src/jp/lionas/alexa/fundra/def/Message.kt
@@ -7,7 +7,7 @@ class Message {
         // メッセージ
         internal const val SKILL_NAME = "Fundra"
         internal const val LAUNCH_SPEECH_TEXT = "ファンドラへようこそ。調べたいファンド名の後に「知りたい」と話してください。"
-        internal const val HELP_SPEECH_TEXT = "調べたいファンド名の後に「知りたい」と話してください。例えば、日経225が知りたい。のように話してください。"
+        internal const val HELP_SPEECH_TEXT = "調べたいファンド名の後に「知りたい」と話してください。例えば、日経が知りたい。のように話してください。"
         internal const val CANCEL_SPEECH_TEXT = "またのご利用をお待ちしております。"
         internal const val NO_SPOKEN = "指定の銘柄の情報はありません"
         internal const val PLUS = "プラス"
